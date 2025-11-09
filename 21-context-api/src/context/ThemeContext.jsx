a8febe3e72c40d9c6d23148f97695b8f9e8f8d19
Anmol Dhiman <170656777+anmolD1himan@users.noreply.github.com>
2025-11-09 14:53:57 +0530

    Contex API Done

---
+++ 21-context-api/src/context/ThemeContext.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import { createContext } from "react"
+
+
+export const themeDataContext = createContext()
+
+const ThemeContext = (props) => {
+    const [theme, setTheme] = useState('light')
+  return (
+    <div>
+       <themeDataContext.Provider value={[theme,setTheme]}>
+            {props.children}
+            </themeDataContext.Provider>
+       
+    </div>
+  )
+}
+export default ThemeContext
